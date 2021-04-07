eb08903c6f8506a10b0421c3ea8a94da2bedb200
incrementals <49661996+incrementals@users.noreply.github.com>
2021-04-07 17:12:18 -0600

    Upgrade!!
    you can now upgrade, cool i guess

--- clicker/main.js
+++ clicker/main.js
@@ -1,19 +1,19 @@
 var points = 0;
-var upgradeTier = 0;
+var buttonLevel = 0;
 
 function getPoints(number){
- points = points + number;
+    points = points + number;
     document.getElementById("points").innerHTML = points;
 };
 
 function upgradePointClick(){
-    var upgradePointClickCost = Math.floor(10 * Math.pow(1.1,upgradeTier));     //works out the cost of this cursor
+    var upgradePointClickCost = Math.floor(10 * Math.pow(1.1,buttonLevel));     //works out the cost of this cursor
     if(points >= upgradePointClickCost){                                   //checks that the player can afford the cursor
-        upgradeTier = upgradeTier + 1;                                   //increases number of upgradeTier
+        buttonLevel = buttonLevel + 1;                                   //increases number of buttonLevel
     	points = points - upgradePointClickCost;                          //removes the points spent
-        document.getElementById('upgradeTier').innerHTML = upgradeTier;  //updates the number of upgradeTier for the user
+        document.getElementById('buttonLevel').innerHTML = buttonLevel;  //updates the number of buttonLevel for the user
         document.getElementById('points').innerHTML = points;  //updates the number of points for the user
     };
-    var nextUpgradePointClickCost = Math.floor(10 * Math.pow(1.1,upgradeTier));       //works out the cost of the next cursor
-    document.getElementById('upgradePointClickCost').innerHTML = nextUpgradePointClickCost;  //updates the cursor cost for the user
+    var nextUpgradePointClickCost = Math.floor(10 * Math.pow(1.1,buttonLevel));       //works out the cost of the next cursor
+    document.getElementById('UPCC').innerHTML = nextUpgradePointClickCost;  //updates the cursor cost for the user
 };
