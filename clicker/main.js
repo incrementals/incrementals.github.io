28517017ab6032f2b0fa61bda130de25d8e1407a
incrementals <49661996+incrementals@users.noreply.github.com>
2021-04-09 21:44:56 -0600

    a
    a

--- clicker/main.js
+++ clicker/main.js
@@ -1,19 +1,25 @@
-var points = 0;
-var buttonLevel = 0;
+var bricks = 0;
+var brickClickerLevel = 0;
+var pointPrinters = 0;
 
-function getPoints(number){
-    points = points + number;
-    document.getElementById("points").innerHTML = points;
+function prettify(input){
+    var output = Math.round(input * 1000)/1000;
+	return output;
+}
+
+function getBricks(number){
+    bricks = bricks + number;
+    document.getElementById("bricks").innerHTML = prettify(bricks);
 };
 
-function upgradePointClick(){
-    var upgradePointClickCost = Math.floor(10 * Math.pow(1.1,buttonLevel));     //works out the cost of this cursor
-    if(points >= upgradePointClickCost){                                   //checks that the player can afford the cursor
-        buttonLevel = buttonLevel + 1;                                   //increases number of buttonLevel
-    	points = points - upgradePointClickCost;                          //removes the points spent
-        document.getElementById('buttonLevel').innerHTML = buttonLevel;  //updates the number of buttonLevel for the user
-        document.getElementById('points').innerHTML = points;  //updates the number of points for the user
+function upgradeBrickClicker(){
+    var upgradeBrickClickerCost = Math.floor(10 * Math.pow(1.1,brickClickerLevel));     //works out the cost of this cursor
+    if(bricks >= upgradeBrickClickerCost){                                   //checks that the player can afford the cursor
+        brickClickerLevel = brickClickerLevel + 1;                                   //increases number of brickClickerLevel
+    	bricks = bricks - upgradeBrickClickerCost;                          //removes the bricks spent
+        document.getElementById('brickClickerLevel').innerHTML = brickClickerLevel;  //updates the number of brickClickerLevel for the user
+        document.getElementById('bricks').innerHTML = prettify(bricks);  //updates the number of bricks for the user
     };
-    var nextUpgradePointClickCost = Math.floor(10 * Math.pow(1.1,buttonLevel));       //works out the cost of the next cursor
-    document.getElementById('UPCC').innerHTML = nextUpgradePointClickCost;  //updates the cursor cost for the user
+    var nextUpgradeBrickClickerCost = Math.floor(10 * Math.pow(1.1,brickClickerLevel));       //works out the cost of the next cursor
+    document.getElementById('UBCC').innerHTML = nextUpgradeBrickClickerCost;  //updates the cursor cost for the user
 };
